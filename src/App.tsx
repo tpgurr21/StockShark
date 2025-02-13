e90d094978143344d51d9973234e31a199398238
Thomas Gurr <tpgurr21@gmail.com>
2025-02-12 21:48:52 -0500

    added API key and endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import Card from './Components/Card/Card';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
+	
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e);
+    }
+
+    const onClick = async(e: SyntheticEvent) => {
+        const result = await searchCompanies(search);
+        // setSearchResult(result.data); THIS WON'T WORK
+        if (typeof result === "string") {
+          setServerError(result);
+        } else if(Array.isArray(result.data)) {
+          setSearchResult(result.data);
+        }
+        console.log(searchResult);
+    };
   return (
     <div className="App">
-      <Search />
-     <CardList />
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      {serverError && <h1>{serverError}</h1>}
+      <CardList />
     </div>
   );
 }
--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,18 +1,13 @@
 import React, { ChangeEvent, useState, SyntheticEvent } from "react";
 
-type Props = {};
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-	const [search, setSearch] = useState<string>("");
+const Search: React.FC<Props> = ({onClick, search, handleChange,}: Props): JSX.Element => {
 	
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    }
-
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e)
-    };
     return (
         <div>
             <input value={search} onChange={(e) => handleChange(e)}></input>
