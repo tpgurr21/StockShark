14e00feccbd7d79e0117a2fd997ac5297bf67dfa
Thomas Gurr <tpgurr21@gmail.com>
2025-02-12 22:48:11 -0500

    refactored search bar, included an addPortfolio button

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,18 @@ function App() {
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>("");
 	
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
     }
 
-    const onClick = async(e: SyntheticEvent) => {
+    const onPortfolioCreate = (e: SyntheticEvent) => {
+      e.preventDefault();
+      console.log(e)
+    }
+
+    const onSearchSubmit = async(e: SyntheticEvent) => {
+        e.preventDefault();
         const result = await searchCompanies(search);
         // setSearchResult(result.data); THIS WON'T WORK
         if (typeof result === "string") {
@@ -28,9 +34,9 @@ function App() {
     };
   return (
     <div className="App">
-      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
       {serverError && <h1>{serverError}</h1>}
-      <CardList />
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
     </div>
   );
 }
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, {SyntheticEvent} from "react";
 import "./Card.css";
-
+import {CompanySearch} from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
 
 interface Props {
-  companyName: string;
-  ticker: string;
-  price: number;
+	id: string;
+	searchResult: CompanySearch;
+	onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ companyName, ticker, price }: Props) : JSX.Element => {
-  return (
-<div className="card">
-    <p>Card</p>
-    <div className="details">
-        <h2>{companyName} ({ticker})</h2>
-        <p>${price}</p>
-    </div>
-    <p className="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-</div>
-  )
-}
+const Card: React.FC<Props> = ({
+	id,
+	searchResult,
+	onPortfolioCreate,
+}: Props): JSX.Element => {
+	return (
+		<div className="card">
+			<img alt="company logo" />
+			<div className="details">
+				<h2>
+					{searchResult.name} ({searchResult.symbol})
+				</h2>
+				<p>{searchResult.currency}</p>
+			</div>
+			<p className="info">
+				{searchResult.exchangeShortName} - {searchResult.stockExchange}
+			</p>
+			<AddPortfolio
+				onPortfolioCreate={onPortfolioCreate}
+				symbol={searchResult.symbol}
+			/>
+		</div>
+	);
+};
 
-export default Card
+export default Card;
