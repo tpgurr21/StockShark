fc2164544c18d85d6f6c0425c68093929aa64d47
Thomas Gurr <tpgurr21@gmail.com>
2025-02-13 18:13:59 -0500

    implemented a way to add stocks to a portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,57 @@
-import { ChangeEvent, SyntheticEvent, useState } from 'react';
-import './App.css';
-import Card from './Components/Card/Card';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
-import { CompanySearch } from './company';
-import { searchCompanies } from './api';
+import {ChangeEvent, SyntheticEvent, useState} from "react";
+import "./App.css";
+import Card from "./Components/Card/Card";
+import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
+import {CompanySearch} from "./company";
+import {searchCompanies} from "./api";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
-	
-    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    }
+	const [search, setSearch] = useState<string>("");
+	const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
+	const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+	const [serverError, setServerError] = useState<string>("");
 
-    const onPortfolioCreate = (e: SyntheticEvent) => {
-      e.preventDefault();
-      console.log(e)
-    }
+	const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value);
+		console.log(e);
+	};
 
-    const onSearchSubmit = async(e: SyntheticEvent) => {
-        e.preventDefault();
-        const result = await searchCompanies(search);
-        // setSearchResult(result.data); THIS WON'T WORK
-        if (typeof result === "string") {
-          setServerError(result);
-        } else if(Array.isArray(result.data)) {
-          setSearchResult(result.data);
-        }
-        console.log(searchResult);
-    };
-  return (
-    <div className="App">
-      <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange}/>
-      {serverError && <h1>{serverError}</h1>}
-      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
-    </div>
-  );
+	const onPortfolioCreate = (e: any) => {
+		e.preventDefault();
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return
+		const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+	};
+
+	const onSearchSubmit = async (e: SyntheticEvent) => {
+		e.preventDefault();
+		const result = await searchCompanies(search);
+		// setSearchResult(result.data); THIS WON'T WORK
+		if (typeof result === "string") {
+			setServerError(result);
+		} else if (Array.isArray(result.data)) {
+			setSearchResult(result.data);
+		}
+		console.log(searchResult);
+	};
+	return (
+		<div className="App">
+			<Search
+				onSearchSubmit={onSearchSubmit}
+				search={search}
+				handleSearchChange={handleSearchChange}
+			/>
+			{serverError && <h1>{serverError}</h1>}
+			<ListPortfolio portfolioValues={portfolioValues} />
+			<CardList
+				searchResults={searchResult}
+				onPortfolioCreate={onPortfolioCreate}
+			/>
+		</div>
+	);
 }
 
 export default App;
